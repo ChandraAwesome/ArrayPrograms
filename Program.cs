f5754da2487bdc0e4acf3dc7755ad439f212bdf4
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-23 17:31:33 +0530

    [Chandra]

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using Patterns._4B_programs;
+
+namespace Patterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Access_Modifiers access= new Access_Modifiers();
+            //Console.WriteLine(access.y); // access the public modifier data
+            //Console.WriteLine(access.z);// we got error coz of z is a protected data type
+            //Console.WriteLine(access.x);// x is an private data type [error]
+            //
+            // Console.WriteLine("Hello, World!");
+            //Triangle.ReverseAngleTriangle();
+            //Triangle.RightAngleTriangle();
+            //Triangle.EquiTringle();
+            //Triangle.ReverseEqiTriangle();
+            //ReverseaNumber.ReverseusingChar();
+            //ReverseaNumber.Reverseusingloop();
+
+            //SumofGivenNumbers.Add();
+            //SumofGivenNumbers.UsingArray();
+
+            //Duplicate_Elements.Count();
+            MaxandMin.MaximumandMinimum();
+
+          //  // ADDING AND DELETING ELEMENTS IN ARRAY USING INT DATA TYPE
+          //  Console.WriteLine("Add the no of elements");
+          //  int size = Convert.ToInt32(Console.ReadLine());
+          //  int[] arr=new int[size];
+          //  //int[] arr = {10,20, 30};
+          //  Console.WriteLine("Enter the elements");
+          ////  Console.WriteLine("Select from above Elements ");
+          //  foreach(int j in arr)
+          //  {
+          //      arr[j] = Convert.ToInt32(Console.ReadLine());
+          //      //Console.WriteLine(j+" ");
+          //  }
+          //  Console.WriteLine("\n Plese pickup one element to delete");
+          //  int delte=Convert.ToInt32(Console.ReadLine());
+          //  DeleteElementss.DeleteElement(arr,delte);
+
+          //  //------------------USING THE DOUBLE DATA TYPE ELEMENTS----------------------------//
+
+          //  Console.WriteLine("Add the no of elements");
+          //  int sizee = Convert.ToInt32(Console.ReadLine());
+          //  int[] arrr = new int[sizee];
+          //  //int[] arr = {10,20, 30};
+          //  Console.WriteLine("Enter the elements");
+          //  //  Console.WriteLine("Select from above Elements ");
+          //  foreach (int j in arrr)
+          //  {
+          //      arr[j] = Convert.ToInt32(Console.ReadLine());
+          //      //Console.WriteLine(j+" ");
+          //  }
+          //  Console.WriteLine("\n Plese pickup one element to delete");
+          //  int delete = Convert.ToInt32(Console.ReadLine());
+          //  DeleteElementss.DeleteElement(arr, delete);
+
+
+        }
+    }
+}
